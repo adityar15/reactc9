ac18b139639146f4d49d0e7ab56837abb5ccf9e8
Aditya Kadam <adityask2194@gmail.com>
2025-01-23 20:21:25 +0000

    react sesion 1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Heading from './components/Heading'
+import Card from './components/Card'
+
+export default function App() {
+  return (
+    <div>
+      <Heading text="First heading" />
+      <Heading text="Second heading"  />
+      <Heading text="Third heading" />
+
+      <Card title="Product 1">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ratione quaerat facere, vero sequi quibusdam provident aliquid totam mollitia. Ratione odit cumque voluptas fuga, laboriosam facere nemo officiis atque neque.
+        <p>asdasd</p>
+        <button>CLick</button>
+      </Card>
+
+    </div>
+  )
+}
