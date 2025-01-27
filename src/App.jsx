dee1a389c8425750b77455c71ad53e83b16c2924
Aditya Kadam <adityask2194@gmail.com>
2025-01-27 20:16:02 +0000

    react session 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Heading from './components/Heading'
 import Card from './components/Card'
+import Counter from './components/Counter'
+import GithubSearch from './components/GithubSearch'
 
 export default function App() {
   return (
     <div>
-      <Heading text="First heading" />
+      {/* <Heading text="First heading" />
       <Heading text="Second heading"  />
       <Heading text="Third heading" />
 
@@ -13,7 +15,10 @@ export default function App() {
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ratione quaerat facere, vero sequi quibusdam provident aliquid totam mollitia. Ratione odit cumque voluptas fuga, laboriosam facere nemo officiis atque neque.
         <p>asdasd</p>
         <button>CLick</button>
-      </Card>
+      </Card> */}
+
+      <Counter />
+      <GithubSearch />
 
     </div>
   )
