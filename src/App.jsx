546e16f3b2c02ba7e506668869b9dfd49911f276
Aditya Kadam <adityask2194@gmail.com>
2025-02-03 20:29:49 +0000

    react session advance updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import { Route, Routes } from 'react-router'
 import GithubSearch from './pages/GithubSearch'
 import Counter from './pages/Counter'
 import Products from './pages/Products'
+import Layout from './components/Layout'
 
 export default function App() {
   return (
     <Routes>
-      <Route index element={<GithubSearch />} />
-      <Route path="/counter" element={<Counter />} />
-      <Route path="/products" element={<Products />} />
+      <Route element={<Layout />}>
+        <Route index element={<GithubSearch />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/products" element={<Products />} />
+      </Route>
+
     </Routes>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center gap-4 p-4">
+        <Link to="/">GithubSearch</Link>
+        <Link to="/counter">Counter</Link>
+        <Link to="/products">Products</Link>
+    </div>
+  )
+}
